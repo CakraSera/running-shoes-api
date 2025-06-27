d405b3fc74f06bc5aab251174e36d3cb9e22b11f
CakraSera <rakelsera2001@gmail.com>
2025-06-27 09:33:38 +0700

    feat: implement Docker setup and enhance shoe data management with CRUD operations

--- data/shoes.ts
+++ data/shoes.ts
@@ -1,42 +1,42 @@
-type Shoe = {
-  id: number;
+export type Shoe = {
+  id: string;
   name: string;
   description: string;
   imageUrl?: string;
-  brand?: string;
+  brand: string;
 };
 
-export const shoes: Shoe[] = [
+export const dataShoes: Shoe[] = [
   {
-    id: 1,
+    id: "1",
     name: "Nike Air Max",
     description: "A comfortable and stylish running shoe.",
     imageUrl: "https://example.com/nike-air-max.jpg",
     brand: "Nike",
   },
   {
-    id: 2,
+    id: "2",
     name: "Adidas Ultraboost",
-    description: "High-performance running shoes with great cushioning.",
+    description: "Highperformance running shoes with great cushioning.",
     imageUrl: "https://example.com/adidas-ultraboost.jpg",
     brand: "Adidas",
   },
   {
-    id: 3,
+    id: "3",
     name: "Puma RS-X",
     description: "Retro-inspired sneakers with a bold design.",
     imageUrl: "https://example.com/puma-rs-x.jpg",
     brand: "Puma",
   },
   {
-    id: 4,
+    id: "4",
     name: "Reebok Classic",
     description: "Timeless sneakers that never go out of style.",
     imageUrl: "https://example.com/reebok-classic.jpg",
     brand: "Reebok",
   },
   {
-    id: 5,
+    id: "5",
     name: "New Balance 990",
     description:
       "Premium running shoes known for their comfort and durability.",
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Hono } from "hono";
-import { shoes } from "../data/shoes";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { nanoid } from "nanoid";
+import { dataShoes, Shoe } from "../data/shoes";
+
+let shoes = dataShoes;
 
 const app = new Hono();
+
+const createPostSchema = z.object({
+  name: z.string().min(3).max(100),
+  description: z.string().min(3).max(100),
+  brand: z.string().min(3).max(100),
+});
 
 app.get("/", (c) => {
   return c.json({ message: "Hello, Hono!" });
@@ -12,12 +23,36 @@ app.get("/shoes", (c) => {
 });
 
 app.get("/shoes/:id", (c) => {
-  const id = parseInt(c.req.param("id"));
-  const shoe = shoes.find((s) => s.id === id);
+  const id = c.req.param("id");
+  const shoe = shoes.find((shoe) => shoe.id === id);
   if (!shoe) {
     return c.json(404);
   }
   return c.json(shoe);
 });
+
+app.post(
+  "/shoes",
+  zValidator("json", createPostSchema, (result, c) => {
+    if (!result.success) {
+      return c.text("Invalid!", 400);
+    }
+  }),
+  async (c) => {
+    const newShoe = await c.req.valid("json");
+    const id = nanoid();
+    const newShoes: Shoe[] = [
+      ...shoes,
+      {
+        id,
+        name: newShoe.name,
+        description: newShoe.description,
+        brand: newShoe.brand,
+      },
+    ];
+    shoes = newShoes;
+    return c.json(newShoes);
+  }
+);
 
 export default app;
