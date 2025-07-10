e6ca53d673de10108bc2edf6e3f05c23d944f1c8
CakraSera <rakelsera2001@gmail.com>
2025-07-11 05:47:20 +0700

    chore: implement PostgreSQL client connection and update TypeScript configuration for ES2023

---
+++ database.ts
@@ -0,0 +1,11 @@
+import * as pg from "pg";
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+await client.connect();
+
+const res = await client.query("SELECT * FROM Brands");
+
+await client.end();
